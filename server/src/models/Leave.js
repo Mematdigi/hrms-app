fda2a5c6fe0f97698dc8523527664a8e73d49b8a
Suraj Chaturvedi <rishabh@mematdigi.com>
2025-11-17 12:25:56 +0530

    hrms_suraj ||latest pull

--- server/src/models/Leave.js
+++ server/src/models/Leave.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const leaveSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  leaveType: { type: String, enum: ['sick', 'casual', 'earned', 'maternity', 'paternity', 'unpaid'], required: true },
+  leaveType: { type: String, enum: ['sick', 'casual', 'earned', 'maternity', 'paternity', 'unpaid','holidays'], required: true },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   numberOfDays: { type: Number, required: true },
